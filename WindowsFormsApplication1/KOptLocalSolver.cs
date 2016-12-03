74340a258fd3d28e39c1dcd850b3453fa0e7ce0c
Skyler Smith <smithskylera@gmail.com>
2016-12-03 09:54:12 -0700

    First draft solution

--- WindowsFormsApplication1/KOptLocalSolver.cs
+++ WindowsFormsApplication1/KOptLocalSolver.cs
@@ -31,6 +31,36 @@ namespace TSP
 
 			//--- Algorithm here ---
 
+			var changed = true;
+			while (changed)
+			{
+				changed = false;
+				for (int i = cities.Length - 1; i >= 0; i--)
+				{
+					for (int j = cities.Length - 1; j >= 0; j--)
+					{
+						ArrayList swapped = Swap(bssf.Route, i, j);
+						ProblemAndSolver.TSPSolution swappedSolution = new ProblemAndSolver.TSPSolution(swapped);
+						if (swappedSolution.costOfRoute() < bssf.costOfRoute())
+						{
+							bssf = swappedSolution;
+							changed = true;//15438
+							System.Console.WriteLine(i + " " + j);
+						}
+					}
+				}
+			}
+
+			//set a 'changed' flag to true
+			//while changed
+			//	for each pair (or 3-set, etc.) of edges
+			//		swap the pair/set
+			//		if the route is valid and its length is better, save it as the new bssf and set changed to true, and break out
+			//Note: This algorithm is short-sighted. It grabs the first route that is better and uses it.
+			//Instead, we could consider getting all of the new routes and setting the best one to the bssf, or keep the top three or four and try them all.
+
+			//--- End algorithm ---
+
 			timer.Stop();
 
 			//When the timer goes off, update results and return bssf
@@ -38,6 +68,31 @@ namespace TSP
 			results[ProblemAndSolver.TIME] = timer.Elapsed.ToString();
 			results[ProblemAndSolver.COUNT] = numUpdates.ToString();
 			return bssf;
+		}
+
+		/// <summary>
+		/// Swap the destinations for the edges coming out of the given cities
+		/// </summary>
+		/// <param name="route">Route. The route in which to swap</param>
+		/// <param name="city1">City1. The first city to swap</param>
+		/// <param name="city2">City2. The second city to swap</param>
+		/// <returns>The new route with the swapped destinations, or null if it is impossible</returns>
+		private ArrayList Swap(ArrayList route, int city1, int city2)
+		{
+			City start1 = (City)route[city1];
+			//City end1 = (City)route[city1 + 1 < route.Count ? city1 + 1 : 0];
+			City start2 = (City)route[city2];
+			//City end2 = (City)route[city2 + 1 < route.Count ? city2 + 1 : 0];
+			//if (double.IsPositiveInfinity(start1.costToGetTo(end2)) || double.IsPositiveInfinity(start2.costToGetTo(end1)))
+			//{
+			//	return null;
+			//}
+			//swap is possible
+			//make a new route with the swapped edges, and return it.
+			ArrayList swapped = (ArrayList)route.Clone();
+			swapped[city1] = start2;
+			swapped[city2] = start1;
+			return swapped;
 		}
 
 		/// <summary>
